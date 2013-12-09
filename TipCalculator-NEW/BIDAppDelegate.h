beef5d5bbd8875ae82b928198dd032eb4413c471
malena mesarina <mmesarina@yahoo.com>
2013-12-09 07:27:49 -0800

    TipCalculator-NEW

---
+++ TipCalculator-NEW/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  TipCalculator-NEW
+//
+//  Created by malena mesarina on 12/8/13.
+//  Copyright (c) 2013 malena mesarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalculator-NEW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator-NEW
+//
+//  Created by malena mesarina on 12/8/13.
+//  Copyright (c) 2013 malena mesarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
